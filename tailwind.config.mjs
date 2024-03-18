9bb18c5df8ae571c9c83ef86c9ced6db0ed080a4
Israel Larrondo <arsistyle@gmail.com>
2024-03-18 17:21:44 -0300

    add: Button component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
 				white: "#fff",
+				dark: "#2d211b",
 				primary: {
 					50: "#f6f5f0",
 					100: "#e9e5d8",
@@ -31,17 +33,18 @@ export default {
 					900: "#3e3b3b",
 					950: "#151414",
 				},
+				whatsapp: "#25D366",
 			},
 			fontFamily: {
 				plusJakarta: ["Plus Jakarta Sans", "sans-serif"],
 			},
 		},
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-    },
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+		},
 	},
 	plugins: [],
 }
